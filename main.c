adfccf663b411b527d77279efde21d50ffd3db89
Shaker Sharabi <sheko100@icloud.com>
2023-04-28 15:20:11 -0700

    Add main.c file

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+
+/**
+ * main - entry point to the shell
+ *
+ * Return: 0
+ */
+
+int main( __attribute__ ((unused))int argc, char *argv[])
+{
+
+	shell(argv[0]);
+
+	return (0);
+}
